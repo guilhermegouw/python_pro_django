ba3718f8ed112e9a0696c10aea8a68fe1d781b8c
Guilherme Gouw <guilhermegouw@Guilhermes-MacBook-Pro.local>
2020-04-02 17:45:26 -0300

    nomeando url home e testando alteração no template.

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,13 +1,22 @@
-from django.test import Client
+import pytest
+from django.urls import reverse
 
 from pypro.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    response = client.get('/')
-    assert response.status_code == 200
+@pytest.fixture
+def resp(client):
+    response = client.get(reverse('home'))
+    return response
 
 
-def test_title(client: Client):
-    response = client.get('/')
-    assert_contains(response, '<title>Python Pro</title>')
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("home")}">Python Pro</a>')
