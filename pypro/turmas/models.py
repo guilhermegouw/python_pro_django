74fea093f637db97349bdef596a18bf6da3d0f4a
guilhermegouw <guilherme.gouw@gmail.com>
2020-04-11 15:17:07 -0300

    primeira parte do app turmas resolvida.

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=64)
+    slug = models.CharField(max_length=64)
+    inicio = models.DateTimeField()
+    fim = models.DateTimeField()
